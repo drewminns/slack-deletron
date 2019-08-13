f87a882b85041b1eeb67efa42d588338ee1a6cac
drewminns <drew.minns@me.com>
2019-08-13 07:15:04 -0400

    Updated process env for production

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,9 +6,6 @@ switch (process.env.NODE_ENV) {
   case 'test':
     path = `${__dirname}/../../.env.test`;
     break;
-  case 'production':
-    path = `${__dirname}/../../.env.production`;
-    break;
   default:
     path = `${__dirname}/../../.env.development`;
 }
