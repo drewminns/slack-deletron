7570ef8e6d3fb5cd3e63184d2f1f1a28f4e02dcb
Drew Minns <drew@playgroundinc.com>
2019-08-21 10:19:40 -0400

    Added Jest testing for express

--- src/server/controllers/Generic.ts
+++ src/server/controllers/Generic.ts
@@ -5,8 +5,22 @@ import { Request, Response } from 'express';
 
 @Controller('api')
 export class GenericAuthController extends Server {
+  public static readonly SUCCESS_MSG = 'hello';
+  public static readonly PARAM_MSG = 'hello ';
+  public static readonly PARAM_MSG_ERROR = 'name not provided';
+
   @Get('')
-  private handleSlackAuth(req: Request, res: Response): void {
-    res.status(200).json({ working: 'perfectly' });
+  private handleRootRequest(req: Request, res: Response): void {
+    res.status(200).json({ message: GenericAuthController.SUCCESS_MSG });
+  }
+
+  @Get('sayhello')
+  private handleParam(req: Request, res: Response): void {
+    const { name } = req.query;
+    if (!name) {
+      res.status(400).send({ error: GenericAuthController.PARAM_MSG_ERROR });
+      return;
+    }
+    res.status(200).json({ message: GenericAuthController.PARAM_MSG + name });
   }
 }
