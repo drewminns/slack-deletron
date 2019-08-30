b68504de81a9ad702b7a10ddcca4bddcfa9bc91c
Drew Minns <drew@playgroundinc.com>
2019-08-30 16:26:44 -0400

    WIP added details for pagination

---
+++ src/server/controllers/SlackFile.ts
@@ -0,0 +1,6 @@
+// https://api.slack.com/methods/files.info
+// https://api.slack.com/methods/files.delete
+// https://api.slack.com/methods/files.list
+
+// This method uses cursor-based pagination to make it easier to incrementally collect information. To begin pagination, specify a limit value under 1000. We recommend no more than 200 results at a time.
+// https://slack.com/api/files.list?limit=200&cursor=c29scl9jdXJzb3JtYXJrOkFvRStabWxzWlY4ME1USXhNalV4TXpjek9UbGZOREkxTXpNM01qVXdOVGsxLjljNWY1NTk0OGE2ZTlmZmY5Yjk3MzZlYjc0ODI4MzJmNjQzNTk0MzRhNTI2YTYyYmYyMjA2OWE0Yjc2ZTEyMjM=
