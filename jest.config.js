d8e3df8a17302c3d3739d4f3f2067b44b2d7d471
Drew Minns <drew@playgroundinc.com>
2019-09-04 10:57:00 -0400

    updating path for circleci ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
+  globals: {
+    'ts-jest': {
+      diagnostics: true,
+    },
+  },
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['<rootDir>/node_modules'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist/'],
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
   coveragePathIgnorePatterns: ['node_modules', 'src/__test__', 'src/server/utils', 'cypress'],
 };
