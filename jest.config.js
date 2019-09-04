aef926f4f17aecb7afe152aab5818e9d6cdd5a3d
Drew Minns <drew@playgroundinc.com>
2019-09-04 11:29:33 -0400

    added more testing for circleci

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      diagnostics: true,
-    },
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
   coveragePathIgnorePatterns: ['node_modules', 'src/__test__', 'src/server/utils', 'cypress'],
 };
